88ee1e1f59ca5832f5f51ffc00e3889cb6176968
Brian Pennington <srsrec@gmail.com>
2018-11-19 09:27:34 -0600

    added Problems as separate class

---
+++ LINQproblems/Problems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQproblems
+{
+    class Problems
+    {
+        List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+        List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+
+        List<string> classGrades = new List<string>()
+{
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+                };
+
+        public void ReturnsTH()
+        {
+            var wordsWithTH = words.
+            }
+    }
+}
